4f1787cda6e32f6798a9bd34c63b3748d76ed2b1
jak09 <mayan1706>
2015-10-01 09:58:37 +0200

    Book : domain class and unit test Comment : domain class unit test

--- test/unit/ivvq/CommentSpec.groovy
+++ test/unit/ivvq/CommentSpec.groovy
@@ -15,6 +15,29 @@ class CommentSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "test a valid comment"(String aUsername, Date aDate, String aComment) {
+        given:"a comment correctly set"
+        Comment comment = new Comment(username: aUsername, date: aDate, comment: aComment);
+
+        expect:"the comment is valid"
+        comment.validate() == true
+
+        where:
+        aUsername    |      aDate      |   aComment
+        "Eva"       |    new Date()   |  "Interesting movie, I love it !"
+    }
+
+    void "test a invalid comment"(String aUsername, Date aDate, String aComment) {
+        given:"a comment incorrectly set"
+        Comment comment = new Comment(username: aUsername, date: aDate, comment: aComment)
+
+        expect:"the comment is invalid"
+        comment.validate() == false
+
+        where:
+        aUsername    |      aDate      |   aComment
+           null     |    new Date()   |  "Interesting movie, I love it !"
+           "Eva"    |    new Date()   |  ""
+           "Eva"    |    new Date()   |  null
     }
 }
