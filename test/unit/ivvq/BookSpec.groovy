e8f4b0c2fe57e645fa45c6f1b7d5120d0ae120fb
yanxkz <mayanxkz@hotmail.com>
2015-10-02 14:02:29 +0200

    unit test modified

--- test/unit/ivvq/BookSpec.groovy
+++ test/unit/ivvq/BookSpec.groovy
@@ -17,36 +17,35 @@ class BookSpec extends Specification {
     }
 
     @Unroll
-    void "test a valid book"(String aName, Date aDateEdition, String aWriter, String aExtrait) {
+    void "test a valid book"(String aName, Date aDateEdition, String aWriter, String aExtrait, String aImage) {
         given:"a book correctly set"
-        Book book = new Book(name:aName, dateEdition: aDateEdition, writer: aWriter, extrait: aExtrait);
+        Book book = new Book(name:aName, dateEdition: aDateEdition, writer: aWriter, extrait: aExtrait, image: aImage);
 
         expect:"the book is valid"
         book.validate() == true;
 
         where:
-        aName        |   aDateEdition  |   aWriter  |   aExtrait
-        "Eva"       |    new Date()   |  "Simon"  |  "Plutot qu'une consciencieuse biographie."
-        "La route" |    new Date()   |  "Cormac" |    null
-        "Word War" |    new Date()   |    "Max"   |    ""
-        "Word "     |    new Date()   |    "Max"   |    ""
+        aName        |   aDateEdition  |   aWriter  |      aImage       |   aExtrait
+        "Eva"       |    new Date()   |  "Simon"  |    "im.jpg"    | "Plutot qu'une consciencieuse biographie."
+        "La route" |    new Date()   |  "Cormac" |    "im.jpg"    |   null
+        "Word War" |    new Date()   |    "Max"   |    "im.jpg"    |   ""
+        "Word "     |    new Date()   |    "Max"   |      null      |   "Plutot qu'une consciencieuse biographie."
     }
 
     @Unroll
-    void "test a invalid book"(String aName, Date aDateEdition, String aWriter, String aExtrait) {
+    void "test a invalid book"(String aName, Date aDateEdition, String aWriter, String aExtrait, String aImage) {
         given:"a book incorrectly set"
-        Book book = new Book(name: aName, date: aDateEdition, writer:aWriter, extrait: aExtrait);
+        Book book = new Book(name: aName, date: aDateEdition, writer:aWriter, extrait: aExtrait, image: aImage);
 
         expect:"the book is invalid"
         book.validate() == false;
 
         where:
-        aName        |   aDateEdition  |   aWriter  |   aExtrait
-         null       |    new Date()   |  "Simon"  |  "Plutot qu'une consciencieuse biographie."
-        "La route" |    null         |  "Cormac" |    null
-        "La route" |    new Date()   |     ""     |    null
-        "Word War" |    new Date()   |    null    |    ""
-           ""       |    new Date()   |    "Max"   |    ""
-           "test"  |    new Date()   |    "Max"   |    ""
+        aName        |   aDateEdition  |   aWriter  |    aImage       |   aExtrait
+         null       |    new Date()   |  "Simon"  |   "im.jpg"   |  "Plutot qu'une consciencieuse biographie."
+        "La route" |    null         |  "Cormac" |   "im.jpg"   |   null
+        "La route" |    new Date()   |     ""     |   "im.jpg"   |   null
+        "Word War" |    new Date()   |    null    |    "im.jpg"  |    ""
+           ""       |    new Date()   |    "Max"   |    "im.jpg"  |    ""
     }
 }
