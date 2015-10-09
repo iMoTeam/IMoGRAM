c9e970d7e963b4521c408583406a9ff6ece9afd5
WeDemBoyz <kmagali120@gmail.com>
2015-10-09 11:26:25 +0200

    UserControllerSpec tests

--- test/unit/ivvq/UserControllerSpec.groovy
+++ test/unit/ivvq/UserControllerSpec.groovy
@@ -1,81 +1,153 @@
 package ivvq
 
-import grails.test.mixin.TestFor
-import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
- */
-@TestFor(User)
+import grails.test.mixin.*
+import spock.lang.*
+
+@TestFor(UserController)
+@Mock(User)
 class UserControllerSpec extends Specification {
 
-    def setup() {
+    def populateValidParams(params) {
+        assert params != null
+        // TODO: Populate valid properties like...
+        params["firstName"] = 'Brother'
+        params["lastName"] = 'Kaka'
+        params["username"] = 'kangaroo'
+        params["email"] = 'yeeree@mailless.com'
+        params["password"] = 'kiki1234'
+        params["profilePhoto"] = null
     }
 
-    def cleanup() {
+    void "Test the index action returns the correct model"() {
+
+        when: "The index action is executed"
+        controller.index()
+
+        then: "The model is correct"
+        !model.userInstanceList
+        model.userInstanceCount == 0
     }
 
-    User user = new User()
+    void "Test the create action returns the correct model"() {
+        when: "The create action is executed"
+        controller.create()
 
-    void "test valid user"() {
-        given:"a user correctly set"
-        user.firstName = "firstname"
-        user.lastName = "lastname"
-        user.username = "toto"
-        user.email = "toto@toto.com"
-        user.password = "pwd1234"
-        user.profilePhoto = "1234567" as byte[]
-
-
-        when:"trying to validate the user"
-        def res = user.validate()
-
-        then:"the user is valid"
-        res == true
-
-        and:"the user no has errors"
-        !user.hasErrors()
+        then: "The model is correctly created"
+        model.userInstance != null
     }
 
-    void "test invalid user"() {
-        given:"a user with a good firstName, lastName, username and password"
-        User user = new User()
-        user.firstName = "firstname"
-        user.lastName = "lastname"
-        user.username = "toto"
-        user.password = "pwd1234"
+    void "Test the save action correctly persists an instance"() {
 
-        and: "with an invalid email"
-        user.email = "totototo.com"
+        when: "The save action is executed with an invalid instance"
+        request.contentType = FORM_CONTENT_TYPE
+        def user = new User()
+        user.validate()
+        controller.save(user)
 
+        then: "The create view is rendered again with the correct model"
+        model.userInstance != null
+        view == 'create'
 
-        when:"trying to validate the user"
-        def res = user.validate()
+        when: "The save action is executed with a valid instance"
+        response.reset()
+        populateValidParams(params)
+        user = new User(params)
 
-        then:"the user is not valid"
-        res == false
+        controller.save(user)
 
-        and:"the user has errors"
-        user.hasErrors()
+        then: "A redirect is issued to the show action"
+        response.redirectedUrl == '/user/show/1'
+        controller.flash.message != null
+        User.count() == 1
     }
 
-    def "test invalid users with where clause"() {
-        given: "a user"
-        user.firstName = aFirstName
-        user.lastName = aLastName
-        user.username = aUsername
-        user.password = aPassword
-        user.email = anEmail
-        user.profilePhoto = aPhoto
+    void "Test that the show action returns the correct model"() {
+        when: "The show action is executed with a null domain"
+        controller.show(null)
 
-        when: "validating the user"
-        def isValid = user.validate()
+        then: "A 404 error is returned"
+        response.status == 404
 
-        then: "the user is not valid"
-        isValid == false
-        where:
-        aFirstName    | aLastName | aUsername | aPassword | anEmail | aPhoto
-         "firstName" |"lastName" | "toto1"   | "toto1234" | "toto"  | "1234567" as byte[]
-        "firstName" | "firstName" | ""        | "toto"    | "toto" | "1234567" as byte[]
+        when: "A domain instance is passed to the show action"
+        populateValidParams(params)
+        def user = new User(params)
+        controller.show(user)
+
+        then: "A model is populated containing the domain instance"
+        model.userInstance == user
+    }
+
+    void "Test that the edit action returns the correct model"() {
+        when: "The edit action is executed with a null domain"
+        controller.edit(null)
+
+        then: "A 404 error is returned"
+        response.status == 404
+
+        when: "A domain instance is passed to the edit action"
+        populateValidParams(params)
+        def user = new User(params)
+        controller.edit(user)
+
+        then: "A model is populated containing the domain instance"
+        model.userInstance == user
+    }
+
+    void "Test the update action performs an update on a valid domain instance"() {
+        when: "Update is called for a domain instance that doesn't exist"
+        request.contentType = FORM_CONTENT_TYPE
+        controller.update(null)
+
+        then: "A 404 error is returned"
+        response.redirectedUrl == '/user/index'
+        flash.message != null
+
+
+        when: "An invalid domain instance is passed to the update action"
+        response.reset()
+        def user = new User()
+        user.validate()
+        controller.update(user)
+
+        then: "The edit view is rendered again with the invalid instance"
+        view == 'edit'
+        model.userInstance == user
+
+        when: "A valid domain instance is passed to the update action"
+        response.reset()
+        populateValidParams(params)
+        user = new User(params).save(flush: true)
+        controller.update(user)
+
+        then: "A redirect is issues to the show action"
+        response.redirectedUrl == "/user/show/$user.id"
+        flash.message != null
+    }
+
+    void "Test that the delete action deletes an instance if it exists"() {
+        when: "The delete action is called for a null instance"
+        request.contentType = FORM_CONTENT_TYPE
+        controller.delete(null)
+
+        then: "A 404 is returned"
+        response.redirectedUrl == '/user/index'
+        flash.message != null
+
+        when: "A domain instance is created"
+        response.reset()
+        populateValidParams(params)
+        def user = new User(params).save(flush: true)
+
+        then: "It exists"
+        User.count() == 1
+
+        when: "The domain instance is passed to the delete action"
+        controller.delete(user)
+
+        then: "The instance is deleted"
+        User.count() == 0
+        response.redirectedUrl == '/user/index'
+        flash.message != null
     }
 }
