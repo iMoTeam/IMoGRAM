d710c1bd5dd53f5349cbc1c09c1e04f9c39dab99
WeDemBoyz <kmagali120@gmail.com>
2015-10-18 20:55:18 +0200

    Created a new branch for issue #5 for removing files committed by mistake, ready to merge #5

--- grails-app/controllers/ivvq/UniversalController.groovy
+++ grails-app/controllers/ivvq/UniversalController.groovy
@@ -4,6 +4,7 @@ class UniversalController {
 
     MovieService movieService
     BookService bookService
+    TVShowService tvShowService
 
 /**
  * Searches movies,books corresponding to a search string passed in params
@@ -13,7 +14,9 @@ class UniversalController {
         params.max = 5
         def moviesList = movieService.searchMovies(params)
         def booksList = bookService.searchBooks(params)
-        def resultsList = moviesList + booksList
-        render(view: 'index', model: [bookInstanceList:booksList, bookInstanceCount: booksList.totalCount,movieInstanceList:moviesList, movieInstanceCount: moviesList.totalCount])
+        def TVShowList = tvShowService.searchTVShow(params)
+        render(view: 'index', model: [bookInstanceList:booksList, bookInstanceCount: booksList.totalCount,
+                                      movieInstanceList:moviesList, movieInstanceCount: moviesList.totalCount,
+                                      tvShowInstanceList:tvShowList, tvShowInstanceCount: tvShowList.totalCount])
     }
 }
