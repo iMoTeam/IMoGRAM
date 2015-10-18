07554d81ff31fb0bc7c65c142572def2ba18b29c
WeDemBoyz <kmagali120@gmail.com>
2015-10-18 16:24:14 +0200

    Linked UniversalController search function to TVShowService search function #5

--- grails-app/controllers/ivvq/UniversalController.groovy
+++ grails-app/controllers/ivvq/UniversalController.groovy
@@ -4,6 +4,7 @@ class UniversalController {
 
     MovieService movieService
     BookService bookService
+    TVShowService tvShowService
 
 /**
  * Searches movies,books corresponding to a search string passed in params
@@ -13,6 +14,9 @@ class UniversalController {
         params.max = 5
         def moviesList = movieService.searchMovies(params)
         def booksList = bookService.searchBooks(params)
-        render(view: 'index', model: [bookInstanceList:booksList, bookInstanceCount: booksList.totalCount,movieInstanceList:moviesList, movieInstanceCount: moviesList.totalCount])
+        def TVShowList = tvShowService.searchTVShow(params)
+        render(view: 'index', model: [bookInstanceList:booksList, bookInstanceCount: booksList.totalCount,
+                                      movieInstanceList:moviesList, movieInstanceCount: moviesList.totalCount,
+                                      tvShowInstanceList:tvShowList, tvShowInstanceCount: tvShowList.totalCount])
     }
 }
