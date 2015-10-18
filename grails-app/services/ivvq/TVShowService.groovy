9de3b829631a11b5681cc83305fec8810b422aa5
WeDemBoyz <kmagali120@gmail.com>
2015-10-18 16:18:49 +0200

    Added TVShow Service class and implemented the search function #5

---
+++ grails-app/services/ivvq/TVShowService.groovy
@@ -0,0 +1,31 @@
+package ivvq
+
+import grails.transaction.Transactional
+
+@Transactional
+class TVShowService {
+
+    def searchTVShow(def params) {
+        String stringToSearch = params['stringToSearch']
+        if(params.stringToSearch != null){
+            stringToSearch = params.stringToSearch
+            stringToSearch = stringToSearch.trim()
+        }
+        def criteria = TVShow.createCriteria()
+        def res = criteria.list (max:params.max,offset:params.offset){
+            if (stringToSearch) {
+                or {
+                    like 'title', "%${stringToSearch}%"
+                    like 'releaseDate', "%${stringToSearch}%"
+                    like 'network', "%${stringToSearch}%"
+                    like 'overview', "%${stringToSearch}%"
+                    like 'country', "%${stringToSearch}%"
+                    like 'imdbID', "%${stringToSearch}%"
+                }
+            }
+        }
+        res
+    }
+
+    }
+
