8773f70a526d1b2d921954986f956c516e756037
Veoth <hugues.odegaard@gmail.com>
2015-11-12 04:29:25 +0100

    Add pagination + correct test #75 close

--- grails-app/services/ivvq/MovieService.groovy
+++ grails-app/services/ivvq/MovieService.groovy
@@ -5,14 +5,14 @@ import grails.transaction.Transactional
 @Transactional
 class MovieService {
 
-    def searchMovies(def params) {
+    def searchMovies(def params, Integer max = 50, Integer offset = 0) {
         String stringToSearch
         if(params.stringToSearch != null){
             stringToSearch = params.stringToSearch
             stringToSearch = stringToSearch.trim()
         }
         def criteria = Movie.createCriteria()
-        def res = criteria.list (max:params.max,offset:params.offset){
+        def res = criteria.list (max: max, offset: offset){
             if (stringToSearch) {
                 or {
                     like 'title', "%${stringToSearch}%"
--- grails-app/services/ivvq/TVShowService.groovy
+++ grails-app/services/ivvq/TVShowService.groovy
@@ -5,14 +5,14 @@ import grails.transaction.Transactional
 @Transactional
 class TVShowService {
 
-    def searchTVShow(def params) {
+    def searchTVShow(def params, Integer max = 50, Integer offset = 0) {
         String stringToSearch = params['stringToSearch']
         if(params.stringToSearch != null){
             stringToSearch = params.stringToSearch
             stringToSearch = stringToSearch.trim()
         }
         def criteria = TVShow.createCriteria()
-        def res = criteria.list (max:params.max,offset:params.offset){
+        def res = criteria.list (max: max, offset: offset){
             if (stringToSearch) {
                 or {
                     like 'title', "%${stringToSearch}%"
